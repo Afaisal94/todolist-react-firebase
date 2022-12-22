015c042ad950f7031691fbb9e5b5c0d7dee05f6a
Afaisal94 <a.faisal1994@gmail.com>
2022-12-22 07:58:31 +0700

    Update Firebase Config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhO7sbKqfKN9jd9xphHEn8VKQIAlIV_Lg",
-  authDomain: "todolist-bfc62.firebaseapp.com",
-  projectId: "todolist-bfc62",
-  storageBucket: "todolist-bfc62.appspot.com",
-  messagingSenderId: "568944783136",
-  appId: "1:568944783136:web:299db71353a1a9126cf145",
-  measurementId: "G-3KDWPDNPEJ",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
